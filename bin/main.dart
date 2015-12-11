19183fc8173c4053a757f4617a90d0966b98ee9b
Daniel Varga <dvarga@skawa.hu>
2015-12-11 18:02:48 +0100

    Ran dartfmt on main and testing pre-commit hook

--- bin/main.dart
+++ bin/main.dart
@@ -3,39 +3,42 @@ import 'package:glob/glob.dart';
 import 'dart:io';
 import 'package:args/args.dart';
 
-const List<String> IGNORED_PATH_PARTS = const [ '.pub', 'packages'];
+const List<String> IGNORED_PATH_PARTS = const ['.pub', 'packages'];
 
 main(List<String> args) {
   var parser = new ArgParser();
-  parser.addOption('report-format', allowed: ['html', 'js'],
-      defaultsTo: 'js', help: 'The format of the output of the analysis');
+  parser.addOption('report-format',
+      allowed: ['html', 'js'],
+      defaultsTo: 'js',
+      help: 'The format of the output of the analysis');
   parser.addOption('analysis-root',
-      defaultsTo: './', help: 'Root path from which all dart files will be analyzed');
+      defaultsTo: './',
+      help: 'Root path from which all dart files will be analyzed');
   var arguments = parser.parse(args);
 
-  var dartFiles = new Glob('**.dart').listSync(
-      root: arguments['analysis-root'],
-      followLinks: false);
+  var dartFiles = new Glob('**.dart')
+      .listSync(root: arguments['analysis-root'], followLinks: false);
   dartFiles.removeWhere((FileSystemEntity entity) {
-    if (entity is! File)
-      return true;
+    if (entity is! File) return true;
     if (entity is File) {
       for (String ignoredPathPart in IGNORED_PATH_PARTS) {
-        if (entity.path.contains(ignoredPathPart))
-          return true;
+        if (entity.path.contains(ignoredPathPart)) return true;
       }
     }
     return false;
   });
 
-  var dartFilePathes = dartFiles.map((FileSystemEntity entity) => entity.path).toList(growable: false);
+  var dartFilePathes = dartFiles
+      .map((FileSystemEntity entity) => entity.path)
+      .toList(growable: false);
   var recorder = new CyclomaticAnalysisRecorder();
   var analyzer = new CyclomaticAnalyzer();
 
-  CyclomaticAnalysisRunner runner = new CyclomaticAnalysisRunner(recorder, analyzer, dartFilePathes);
+  CyclomaticAnalysisRunner runner =
+      new CyclomaticAnalysisRunner(recorder, analyzer, dartFilePathes);
   runner.run();
   AnalysisReporter reporter;
-  switch(arguments['report-format']){
+  switch (arguments['report-format']) {
     case 'html':
       reporter = new HtmlReporter(runner);
       break;
@@ -43,7 +46,8 @@ main(List<String> args) {
       reporter = new JsonReporter(runner);
       break;
     default:
-      throw new ArgumentError.value(arguments['report-format'], 'report-format');
+      throw new ArgumentError.value(
+          arguments['report-format'], 'report-format');
   }
   stdout.write(reporter.getReport());
 }
